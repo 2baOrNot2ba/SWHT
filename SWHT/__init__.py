44b24c249d8d91574f8d0b4b2654037b2d0ef328
Tobia Carozzi <tobia@chalmers.se>
2024-08-22 11:28:12 +0200

    Bumped to v0.10.1

--- SWHT/__init__.py
+++ SWHT/__init__.py
@@ -2,6 +2,6 @@
 A module interface to SWHT functions
 """
 
-__version__ = '0.10.0' #this needs to be kept up to date with setup.py
+__version__ = '0.10.1' #this needs to be kept up to date with setup.py
 
 from SWHT import ecef, display, fileio, ft, lofarConfig, swht, util, Ylm
